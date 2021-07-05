e58c43878fb25579229576fb416b4c0a25b000f8
Ro.Martynov <Umxometal>
2021-07-05 03:00:45 +0300

    hw01-gradle fizzBuzz with guava library

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -1,9 +1,13 @@
 package ru.otus;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.base.Optional;
 
-import static com.google.common.collect.Lists.transform;
+import static com.google.common.base.Optional.fromNullable;
 import static com.google.common.base.Joiner.on;
+import static com.google.common.collect.ImmutableList.of;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.Lists.transform;
+import static java.util.stream.IntStream.rangeClosed;
 
 /**
  * To start the application:
@@ -15,8 +19,21 @@ import static com.google.common.base.Joiner.on;
  */
 public class HelloOtus {
     public static void main(String[] args) {
-        var listItems = ImmutableList.of("Hello", "otus");
+        var listItems = of("Hello", "otus");
         var upperCaseStrings = transform(listItems, String::toUpperCase);
         System.out.println(on(" ").join(upperCaseStrings));
+        System.out.println(playFizzBuzz(fromNullable(1), fromNullable(100)));
+    }
+
+    private static String playFizzBuzz(Optional<Integer> startIndexOpt, Optional<Integer> endIndexOpt) {
+        if (!startIndexOpt.isPresent() || !endIndexOpt.isPresent()) {
+            throw new RuntimeException("Start index value and end index value should be present");
+        }
+        var transformedList
+                = transform(rangeClosed(startIndexOpt.get(), endIndexOpt.get())
+                        .boxed()
+                        .collect(toImmutableList()),
+                number -> number % 5 == 0 ? (number % 7 == 0 ? "FizzBuzz" : "Fizz") : (number % 7 == 0 ? "Buzz" : number));
+        return on(", ").join(transformedList);
     }
 }
